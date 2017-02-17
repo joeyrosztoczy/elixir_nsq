7b12df31fb3f9e334130ad81c27e491ade15cc38
Joey Rosztoczy <joeyr@stotzeq.com>
2017-02-16 18:47:26 -0700

    bumb poison for compatability

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule ElixirNsq.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:poison, "~> 1.5.0"},
+      {:poison, "~> 2.0.0"},
       {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.2"},
       {:httpotion, "~> 2.1.0"},
       {:uuid, "~> 1.1.2"},
